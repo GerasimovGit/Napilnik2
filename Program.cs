07ab75538833c2f9bfedf73fcded3a9975059901
DmitryWork <gerasimovdmitry92@mail.ru>
2022-08-13 11:16:42 +0300

    init

--- Program.cs
+++ Program.cs
@@ -1,9 +1,20 @@
-﻿namespace NapilnikTasks
+﻿using System;
+
+namespace NapilnikTasks
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+        }
+        
+        public static int IndexOf(int[] array, int element)
+        {
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] == element)
+                    return i;
+
+            return -1;
         }
     }
 }
